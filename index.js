33a6bb44ec00247edd3af53235b2f7e21e018cd4
riccardo <riccagallo67@gmail.com>
2022-04-06 17:19:09 +0200

    vai

---
+++ index.js
@@ -0,0 +1,24 @@
+function crittografia(){
+var alfabeto ="abcdefghijklmnopqrstuvwxyz";
+var testochiaro=document.getElementById("testochiaro").value;
+var testocifrato="";
+var testodecifrato="";
+var chiave= document.getElementById("chiave").value;
+
+console.log(testochiaro);
+console.log(chiave);
+
+for(var i=0; i<testochiaro.length;i++){
+    var posizione= alfabeto.indexOf(testochiaro[i]);
+    testocifrato +=alfabeto[(posizione + chiave) % 26];
+}
+document.getElementById("testoc").innerHTML=testocifrato;
+
+
+for(var i=0; i<testochiaro.length;i++){
+    var posizione = alfabeto.indexOf(testocifrato[i]);
+    testodecifrato += alfabeto[(posizione - chiave + 26) % 26];
+}
+
+document.getElementById("testod").innerHTML=testodecifrato;
+}

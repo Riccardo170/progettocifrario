1c1c92b4ae84bc20401576f1b041adae17076177
riccardo <riccagallo67@gmail.com>
2022-04-06 18:25:39 +0200

    finito

--- cifrario.js
+++ cifrario.js
@@ -1,23 +1,28 @@
 function crittografia(){
 var alfabeto ="abcdefghijklmnopqrstuvwxyz";
 var testochiaro=document.getElementById("testochiaro").value;
+testochiaro=testochiaro.replaceAll(" ","");//metodo per rimuove gli spazi
 var testocifrato="";
 var testodecifrato="";
-var chiave= document.getElementById("chiave").value;
-
-console.log(testochiaro);
-console.log((posizione + chiave) % 26);
+var chiavi = document.getElementById("chiavi").value;
+const array=chiavi.split(";");
+var ichiave=0;
 
 for(var i=0; i<testochiaro.length;i++){
     var posizione= alfabeto.indexOf(testochiaro[i]);
-    testocifrato +=alfabeto[(posizione + chiave) % 26];
+    testocifrato +=alfabeto[(posizione + parseInt(array[ichiave])) % 26];
+    ichiave=(ichiave+1) % array.length;
 }
 document.getElementById("testoc").innerHTML=testocifrato;
 
+ichiave=0;
 
-for(var i=0; i<testochiaro.length;i++){
-    var posizione = alfabeto.indexOf(testocifrato[i]);
-    testodecifrato += alfabeto[(posizione - chiave + 26) % 26];
+for(var i=0; i<testocifrato.length;i++){
+    var posizione= alfabeto.indexOf(testocifrato[i]);
+    testodecifrato +=alfabeto[(posizione - parseInt(array[ichiave])+ 26 ) % 26];
+    console.log((posizione - parseInt(array[ichiave])+ 26 ) % 26);
+    ichiave=(ichiave+1) % array.length;
+    
 }
 
 document.getElementById("testod").innerHTML=testodecifrato;
